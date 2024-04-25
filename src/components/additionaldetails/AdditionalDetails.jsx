364b68d68d7abe5b0000addba66e7e7077266b4d
Jean <jmoncayo@pursuit.org>
2024-04-25 15:32:42 -0400

    More Details Popout update

--- src/components/additionaldetails/AdditionalDetails.jsx
+++ src/components/additionaldetails/AdditionalDetails.jsx
@@ -7,7 +7,7 @@ import React from "react";
 const AdditionalDetails = ({ student }) => {
   if (!student) return null;
   
-  const { certifications, codewars } = student;
+  const { names, username, dob, profilePhoto, certifications, codewars } = student;
 
   const isOnTrack = (
     certifications &&
@@ -21,11 +21,22 @@ const AdditionalDetails = ({ student }) => {
   );
 
   return (
-    <div className="additional-details">
+    
+   
+<div className='pop-out-window'>    
+      <div className="student-info pop-out">
+        <img src={profilePhoto} alt={names.preferredName} className="profile-photo" />
+        <h2>{names.preferredName} {names.middleName} {names.surname}</h2>
+      </div>
+      <div className="email-bday">
+        <p>{username}</p>
+        Birthday {dob}
+      </div>
+       <div className="additional-details">
       <h2>Additional Details</h2>
       <p>Codewars Score: {codewars.current.total}</p>
       <p>On-track Status: {isOnTrack ? 'On Track' : 'Off Track'}</p>
-
+    </div>
     </div>
   );
 };
